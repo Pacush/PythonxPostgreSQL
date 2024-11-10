15ca7ca652ee5e988f2f1a44552d31de6ec1d847
José Sánchez <josealejandro.sancheznaranjo@gmail.com>
2024-11-10 00:20:22 -0600

    Funcion calendarios incorporada

--- gui_test.py
+++ gui_test.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
-from tkcalendar import Calendar
+
 import psycopg2
 from PIL import Image, ImageTk
+from tkcalendar import Calendar
 
 
 # Conexión a la base de datos
@@ -744,6 +745,50 @@ def abrir_ventana_citas_empleados():
         connection.close()
     
     centrar_ventana(ventana_citas_empleados, 2, 2, 3)
+
+
+def obtener_cita(calendario, caja_horas):
+    # Obtener la fecha seleccionada en el calendario
+    selected_date = calendario.selection_get()
+
+    # Obtener la hora seleccionada (en formato 1, 2, 3, etc.) y agregar ":00" para mostrar la hora completa
+    time = caja_horas.get()
+    time = time[0]
+    
+    selected_time = f"{time}:00"
+
+    # Combinar la fecha y hora seleccionadas
+    #appointment = f"Cita agendada para: {selected_date} a las {selected_time}"
+
+    # Mostrar la cita agendada
+    #appointment_label.config(text=appointment)
+
+def abrir_ventana_calenario_citas():
+    # Crear la ventana principal
+    ventana_calendario = tk.Tk()
+    ventana_calendario.title('Registrar cita')
+
+    # Calendario
+    cal = Calendar(ventana_calendario, selectmode='day', year=2024, month=11, day=20)
+    cal.pack(pady=20)
+
+    # Reloj (hora)
+    hour_label = tk.Label(ventana_calendario, text="Hora:")
+    hour_label.pack(pady=5)
+
+    # Combobox para seleccionar la hora (en formato 1:00, 2:00, ..., 12:00)
+    hour_combobox = ttk.Combobox(ventana_calendario, values=[f"{i}:00 - {i + 1}:00" for i in range(9, 20)], width=12)
+    hour_combobox.set("9:00 - 10:00")  # valor por defecto
+    hour_combobox.pack(pady=20)
+
+    # Botón para agendar cita
+    tk.Button(ventana_calendario, text='Agendar Cita', command=lambda: obtener_cita(cal, hour_combobox)).pack(pady=20)
+
+    # Etiqueta para mostrar la cita agendada
+    appointment_label
+    appointment_label = tk.Label(ventana_calendario, text="", font=("Helvetica", 12))
+    appointment_label.pack(pady=20)
+
 
 
 def cerrar_sesion():
