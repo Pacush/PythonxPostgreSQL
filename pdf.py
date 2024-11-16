d43abaa20f4800f1f2c3a51894a4e56cafc65c68
José Sánchez <josealejandro.sancheznaranjo@gmail.com>
2024-11-16 00:02:28 -0600

    Ajustes

--- pdf.py
+++ pdf.py
@@ -17,7 +17,6 @@ estatura_paciente = 1.70
 sexo_paciente = "MASCULINO"
 direccion_paciente = "NAPOLES 95"
 telefono_paciente = "3331563135"
-fecha_consulta = "2024-05-05"
 
 diagnostico = "Hola, esto es una prueba."
 
@@ -27,13 +26,12 @@ presentacion_medicamento = "Tableta"
 fecha_cad_medicamento = "2025-05-05"
 nombre_doctor = "Josue Lugo"
 
-
-nombre_archivo = f"consulta_{fecha_consulta}.pdf"
+nombre_archivo = f"./Recetas de consultas/consulta_{nombre_paciente}.pdf"
 
 context = {'ruta_logo': ruta_absoluta, 'nombre_paciente': nombre_paciente, 'edad_paciente': edad_paciente,
         'estatura_paciente': estatura_paciente, 'sexo_paciente': sexo_paciente,
         'direccion_paciente': direccion_paciente, 'telefono_paciente': telefono_paciente,
-        'fecha_consulta': fecha_consulta, 'diagnostico': diagnostico,'nombre_medicamento': nombre_medicamento,
+        'diagnostico': diagnostico,'nombre_medicamento': nombre_medicamento,
         'via_adm_medicamento': via_adm_medicamento, 'presentacion_medicamento': presentacion_medicamento,
         'fecha_cad_medicamento': fecha_cad_medicamento, 'nombre_doctor': nombre_doctor}
 
